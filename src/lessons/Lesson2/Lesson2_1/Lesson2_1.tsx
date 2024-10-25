c01d5fe379b4a6f127f926077bb6347ba59e7f4b
AbizUser <aman.biz.t23@gmail.com>
2024-10-25 21:37:17 +0900

    useEffect Practice

--- src/lessons/Lesson2/Lesson2_1/Lesson2_1.tsx
+++ src/lessons/Lesson2/Lesson2_1/Lesson2_1.tsx
@@ -1,19 +1,39 @@
-const Lesson2_1 = () => {
-  return (
-    <div
-      style={{
-        position: "absolute",
-        backgroundColor: "blue",
-        borderRadius: "50%",
-        opacity: 0.6,
-        pointerEvents: "none",
-        left: -20,
-        top: -20,
-        width: 50,
-        height: 50,
-      }}
-    ></div>
-  );
-};
+// import { useEffect, useState } from "react";
 
-export default Lesson2_1;
+// const Lesson2_1 = () => {
+
+// const [position, setPosition] = useState({ X: 0, y: 0 });
+ 
+// useEffect(() => {
+//   function handleMove(e) {
+//     setPosition({ x: e.clientX, y: e.clientY });
+//   }
+
+//   window.addEventListener("pointermove", handleMove);
+//   //イベントリスナーはクリーンアップしなければ走り続ける
+
+//   return () => {
+//     window.removeEventListener("pointermove", handleMove);
+//   };
+// }, []);
+// //第２依存配列を空にするとレンダリングは一度しか実行しない。
+
+//   return (
+//     <div
+//       style={{
+//         position: "absolute",
+//         backgroundColor: "blue",
+//         borderRadius: "50%",
+//         opacity: 0.6,
+//         pointerEvents: "none",
+//         transform: `translate(${position.x}px, ${position.y}px)`,
+//         left: -20,
+//         top: -20,
+//         width: 50,
+//         height: 50,
+//       }}
+//     ></div>
+//   );
+// };
+
+// export default Lesson2_1;
--- src/lessons/Lesson2/Lesson2_2/Lesson2_2.tsx
+++ src/lessons/Lesson2/Lesson2_2/Lesson2_2.tsx
@@ -1,7 +1,35 @@
+import { useEffect, useState } from "react";
+import { fetchBio } from "./fetchBio";
+
 const Lesson2_2 = () => {
+  const [person, setPerson] = useState<string>("ShinCode");
+  const [bio, setBio] = useState<string | null>(null);
+  const [count, setCount] = useState(0);
+  //無限ループ用
+
+  useEffect(() => {
+    let ignore = false;
+      const startFetching = async () => {
+        const response = await fetchBio(person);
+        // console.log(response)
+        if (!ignore) {
+          setBio(response)
+        }
+      };
+      startFetching();
+      
+      setCount(count + 1);
+
+      return () => {
+        ignore = true;
+      };
+      //Cleanup関数
+    }, [person]);
+    //personの値が変更される度にuseEffectの値が変更される。また依存配列にcountを含めると無限ループが発生
+
   return (
     <div>
-      <select>
+      <select onChange={(e) => setPerson(e.target.value)} value={person}>
         <option value="ShinCode">ShinCode</option>
         <option value="TestUser">TestUser</option>
         <option value="SampleUser">SampleUser</option>
@@ -9,7 +37,8 @@ const Lesson2_2 = () => {
 
       <hr />
 
-      <p className="text-black">{"Loading..."}</p>
+      <p className="text-black">{bio ?? "Loading..."}</p>
+      <p>{count}</p>
     </div>
   );
 };
