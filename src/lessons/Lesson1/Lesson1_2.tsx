14c7d560276038701bcf46d25a4d1e2f381b6140
AbizUser <aman.biz.t23@gmail.com>
2024-10-25 20:24:06 +0900

    Form practice patern1

--- src/lessons/Lesson1/Lesson1_2.tsx
+++ src/lessons/Lesson1/Lesson1_2.tsx
@@ -1,23 +1,63 @@
+import { ChangeEvent, useState } from "react";
+
 const Lesson1_2 = () => {
+    const [form, setForm] = useState({
+      firstName: "code",
+      lastName: "co",
+      email: "@gmail.com",
+    })
   return (
     <div>
       <div className="flex mb-5">
         <label>
           First Name:
-          <input type="text" className="border border-slate-500" />
+          <input
+          onChange={(e: ChangeEvent<HTMLInputElement>) => 
+            setForm(
+              {
+              firstName: e.target.value,
+              lastName: form.lastName,
+              email: form.email,
+            })
+          } 
+          type="text" 
+          className="border border-slate-500" />
         </label>
         <label>
           Last Name:
-          <input type="text" className="border border-slate-500" />
+          <input
+          onChange={(e: ChangeEvent<HTMLInputElement>) => 
+            setForm(
+              {
+              firstName: form.firstName,
+              lastName: form.lastName,
+              email: e.target.value,
+            })
+          } 
+          type="text" 
+          className="border border-slate-500" />
         </label>
         <label>
           Email:
-          <input type="text" className="border border-slate-500" />
+          <input
+          onChange={(e: ChangeEvent<HTMLInputElement>) => 
+            setForm(
+              {
+              firstName: form.firstName,
+              lastName: form.lastName,
+              email: e.target.value,
+            })
+          } 
+          type="text" 
+          className="border border-slate-500" />
         </label>
       </div>
       <p>
+        {form.firstName}
         <br />
+        {form.lastName}
         <br />
+        {form.email}
       </p>
     </div>
   );
